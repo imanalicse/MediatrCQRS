54c0025b26ed46949e89d5cb3c38dcb62849400f
iman <iman@bitmascot.com>
2018-07-31 16:38:02 +0600

    customer command query update

--- MediatrCQRS/Models/CreateCustomerCommand.cs
+++ MediatrCQRS/Models/CreateCustomerCommand.cs
@@ -3,8 +3,7 @@
 namespace MediatrCQRS.Models
 {
     public class CreateCustomerCommand : IRequest<CustomerViewModel>
-    {
-        public int Id { get; set; }
+    {        
         public string Name { get; set; }
     }
 }
